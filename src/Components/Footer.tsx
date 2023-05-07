8a4cd55a2ec69cb50708228267f8000fa84c035f
MatheusReisBarros <matheusrreiss328@gmail.com>
2023-05-07 17:29:54 -0400

    Ajuste footer

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -6,17 +6,17 @@ import { faFacebook,  faTwitter,  faInstagram } from "@fortawesome/free-brands-s
 const Footer = () => {
   return (    
     <footer className="bg-[#ffb803] ">
-      <section className="grid grid-cols-2 pt-4 w-11/12">
+      <section className="grid md:flex md:place-content-around grid-cols-2 pt-4 md:pt-0 w-11/12 items-baseline">
         <div className="flex flex-col">
-          <h2>SITEMAP</h2>
+          <h2 className="font-medium">SITEMAP</h2>
           <Link to="/" className="hover:text-white">HOME</Link>
           <Link to="about" className="hover:text-white">ABOUT</Link>
           <Link to="package" className="hover:text-white">PACKAGE</Link>
-          <Link to="gallery" className="hover:text-white">GALlERY</Link>
+          <Link to="gallery" className="hover:text-white">GALLERY</Link>
           <Link to="contact" className="hover:text-white">CONTACT</Link>
         </div>
         <div className="flex flex-col">
-          <h2>PRODUC</h2>
+          <h2 className="font-medium">PRODUC</h2>
           <ul>
             <li><a href="#" className="hover:text-white">Downloads</a></li>
             <li><a href="#" className="hover:text-white">Instruments</a></li>
@@ -26,7 +26,7 @@ const Footer = () => {
           </ul>
         </div>
         <div className="mt-4">
-          <h2>HELP</h2>
+          <h2 className="font-medium">HELP</h2>
           <ul>
             <li><a href="#" className="hover:text-white">FAQ</a></li>
             <li><a href="#" className="hover:text-white">Network Status</a></li>
@@ -34,7 +34,7 @@ const Footer = () => {
           </ul>
         </div>
         <div className="mt-4">
-          <h2 className="">SUBSCRIBE</h2>
+          <h2 className="font-medium">SUBSCRIBE</h2>
           <label>
             <input
               className="bg-white w-8/12"
